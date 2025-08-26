01309bb35b1f786ac40f6d58ec0df6188e6c490a
Aidan Tobar <atobar@bgsu.edu>
2025-08-26 15:24:08 -0400

    updated resume

--- app/(pages)/resume/page.tsx
+++ app/(pages)/resume/page.tsx
@@ -33,7 +33,7 @@ export default function Resume() {
       />
       <div className="flex justify-center pt-20 z-10">
         <iframe
-          src="/text-doc/July2025Resume.pdf"
+          src="/text-doc/Updated_Resume_Fall2025.pdf"
           title="Resume"
           className=" md:w-3/4 xl:w-3/4 2xl:w-2/4 sm:w-full lg:w-3/4 resume z-10"
         />
